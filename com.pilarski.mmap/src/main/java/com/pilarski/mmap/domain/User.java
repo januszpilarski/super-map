867c7a633b2ee1002d8c54f06ebb2da097d7c723
januszap <dingo5017@gmail.com>
2017-02-18 10:32:39 +0100

    new

---
+++ com.pilarski.mmap/src/main/java/com/pilarski/mmap/domain/User.java
@@ -0,0 +1,99 @@
+package com.pilarski.mmap.domain;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class User {
+
+	public User(String username, String password, String email) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_id")
+	private Long id;
+	private String username;
+	private String password;
+	private String email;
+
+	@ManyToMany
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles;
+
+	@OneToMany(mappedBy = "user")
+	private List<Place> places;
+
+	@OneToMany(mappedBy = "user")
+	private List<PointAndComment> pointsAndComments;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public List<Place> getPlaces() {
+		return places;
+	}
+
+	public void setPlaces(List<Place> places) {
+		this.places = places;
+	}
+
+	public List<PointAndComment> getPointsAndComments() {
+		return pointsAndComments;
+	}
+
+	public void setPointsAndComments(List<PointAndComment> pointsAndComments) {
+		this.pointsAndComments = pointsAndComments;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
