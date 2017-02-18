9854333181b86b1da8ffe727d9e85565a1bc191d
januszap <dingo5017@gmail.com>
2017-02-18 10:33:13 +0100

    new

---
+++ com.pilarski.mmap/src/main/java/com/pilarski/mmap/MmapMvcApplication.java
@@ -0,0 +1,18 @@
+package com.pilarski.mmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MmapMvcApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
