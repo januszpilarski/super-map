5271f99b45924549f57f161d7578adc21edde4e6
januszap <dingo5017@gmail.com>
2017-02-18 10:31:03 +0100

    relation userid

--- com.pilarski.mmap/src/main/java/com/pilarski/mmap/domain/PointAndComment.java
+++ com.pilarski.mmap/src/main/java/com/pilarski/mmap/domain/PointAndComment.java
@@ -14,6 +14,22 @@ public class PointAndComment {
 	public PointAndComment() {
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name="pointAndComment_id")
@@ -24,8 +40,10 @@ public class PointAndComment {
 	@ManyToOne
 	@JoinColumn(name="place_id")
 	Place place;
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	User user;
 	
-
 	public Place getPlace() {
 		return place;
 	}
