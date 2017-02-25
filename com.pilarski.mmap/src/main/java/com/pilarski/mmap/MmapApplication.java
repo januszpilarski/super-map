bd8e6f646c00d13a939a0d8f9b69b330b285370d
januszap <dingo5017@gmail.com>
2017-02-25 19:54:31 +0100

    new

---
+++ com.pilarski.mmap/src/main/java/com/pilarski/mmap/MmapApplication.java
@@ -0,0 +1,13 @@
+package com.pilarski.mmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmapApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(MmapApplication.class, args);
+	}
+
+}
